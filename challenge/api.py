6c35f739c96435fbb5d7b0fc9207f1955104d136
jvillablanca02 <jvillablanca02@outlook.com>
2024-09-11 10:11:00 -0300

    Update api.py
    Validación de Datos: Utiliza enumeraciones y restricciones de Pydantic para asegurar que los datos de entrada sean correctos.
    Preprocesamiento: Incluye un paso de preprocesamiento antes de realizar la predicción, lo que puede mejorar la precisión del modelo.
    Manejo de Excepciones: Utiliza un bloque try-except para manejar excepciones durante la predicción y registrar los errores.
    Ejecución con Uvicorn: Incluye un bloque para ejecutar la aplicación con Uvicorn, lo que facilita el despliegue.

--- challenge/api.py
+++ challenge/api.py
@@ -1,31 +1,87 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
-import numpy as np
+import logging
+from enum import Enum
+from typing import List
 
-# Cargar el modelo entrenado
-model = joblib.load("challenge/finalized_model.pkl")
+import pandas as pd
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, conint
 
-# Crear la aplicación FastAPI
+from challenge.model import DelayModel
+
 app = FastAPI()
 
-# Definir el modelo de datos para la solicitud de predicción
-class PredictionRequest(BaseModel):
-    features: list
+# Inicializar el modelo a nivel de módulo, pero solo cuando se necesite por primera vez
+model = None
 
-# Endpoint para verificar el estado de la API
+def get_model():
+    global model
+    if model is None:
+        model = DelayModel()
+    return model
+
+class FlightType(str, Enum):
+    NATIONAL = "N"
+    INTERNATIONAL = "I"
+
+class Airline(str, Enum):
+    AMERICAN_AIRLINES = "American Airlines"
+    AIR_CANADA = "Air Canada"
+    AIR_FRANCE = "Air France"
+    AEROMEXICO = "Aeromexico"
+    AEROLINEAS_ARGENTINAS = "Aerolineas Argentinas"
+    AUSTRAL = "Austral"
+    AVIANCA = "Avianca"
+    ALITALIA = "Alitalia"
+    BRITISH_AIRWAYS = "British Airways"
+    COPA_AIR = "Copa Air"
+    DELTA_AIR = "Delta Air"
+    GOL_TRANS = "Gol Trans"
+    IBERIA = "Iberia"
+    KLM = "K.L.M."
+    QANTAS_AIRWAYS = "Qantas Airways"
+    UNITED_AIRLINES = "United Airlines"
+    GRUPO_LATAM = "Grupo LATAM"
+    SKY_AIRLINE = "Sky Airline"
+    LATIN_AMERICAN_WINGS = "Latin American Wings"
+    PLUS_ULTRA_LINEAS_AEREAS = "Plus Ultra Lineas Aereas"
+    JETSMART_SPA = "JetSmart SPA"
+    OCEANAIR_LINHAS_AEREAS = "Oceanair Linhas Aereas"
+    LACSA = "Lacsa"
+
+class FlightData(BaseModel):
+    OPERA: Airline
+    TIPOVUELO: FlightType
+    MES: conint(ge=1, le=12) = Field(..., description="Mes (1-12)")
+
+class FlightsRequest(BaseModel):
+    flights: List[FlightData]
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400, content={"detail": exc.errors(), "body": exc.body}
+    )
+
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
     return {"status": "OK"}
 
-# Endpoint para realizar predicciones
 @app.post("/predict", status_code=200)
-async def post_predict(request: PredictionRequest) -> dict:
-    features = np.array(request.features).reshape(1, -1)
-    prediction = model.predict(features)
-    return {"prediction": int(prediction[0])}
+async def post_predict(request: FlightsRequest) -> dict:
+    try:
+        model = get_model()  # Obtener o inicializar el modelo
+        flight_data = pd.DataFrame([flight.dict() for flight in request.flights])
+        preprocessed_data = model.preprocess(flight_data)
+        predictions = model.predict(preprocessed_data)
+        return {"predict": predictions}
+    except Exception as e:
+        logging.exception("Ocurrió un error durante la predicción")
+        raise HTTPException(status_code=500, detail="Ocurrió un error interno")
 
 # Ejecutar la aplicación con Uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
