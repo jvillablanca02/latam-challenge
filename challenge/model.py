1389df8bf251c83fbcf913f651afdc7806f24521
jvillablanca02 <jvillablanca02@outlook.com>
2024-09-12 06:05:52 -0300

    Update model.py

--- challenge/model.py
+++ challenge/model.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,19 +8,79 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 import xgboost as xgb
-from xgboost import XGBClassifier, plot_importance
-from sklearn.utils.class_weight import compute_class_weight
-from typing import Union, Tuple, List
-from pathlib import Path
-import logging
-import json
+from xgboost import plot_importance
 
 warnings.filterwarnings('ignore')
 
-# Cargar Datos
+# 0. Cargar Datos
 data = pd.read_csv('./data/data.csv')
+data.info()
 
-# Generación de Características
+# 1. Análisis de Datos: Primera Vista
+# Distribución de vuelos por aerolínea
+flights_by_airline = data['OPERA'].value_counts()
+plt.figure(figsize=(10, 2))
+sns.set(style="darkgrid")
+sns.barplot(flights_by_airline.index, flights_by_airline.values, alpha=0.8)
+plt.title("Vuelos por Aerolínea")
+plt.ylabel("Vuelos", fontsize=12)
+plt.xlabel("Aerolínea", fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
+
+# Distribución de vuelos por día
+flights_by_day = data['DIA'].value_counts()
+plt.figure(figsize=(10, 2))
+sns.set(style="darkgrid")
+sns.barplot(flights_by_day.index, flights_by_day.values, color="lightblue", alpha=0.8)
+plt.title("Vuelos por Día")
+plt.ylabel("Vuelos", fontsize=12)
+plt.xlabel("Día", fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
+
+# Distribución de vuelos por mes
+flights_by_month = data['MES'].value_counts()
+plt.figure(figsize=(10, 2))
+sns.set(style="darkgrid")
+sns.barplot(flights_by_month.index, flights_by_month.values, color="lightblue", alpha=0.8)
+plt.title("Vuelos por Mes")
+plt.ylabel("Vuelos", fontsize=12)
+plt.xlabel("Mes", fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
+
+# Distribución de vuelos por día de la semana
+flights_by_day_in_week = data['DIANOM'].value_counts()
+days = [
+    flights_by_day_in_week.index[2],
+    flights_by_day_in_week.index[5],
+    flights_by_day_in_week.index[4],
+    flights_by_day_in_week.index[1],
+    flights_by_day_in_week.index[0],
+    flights_by_day_in_week.index[6],
+    flights_by_day_in_week.index[3]
+]
+values_by_day = [
+    flights_by_day_in_week.values[2],
+    flights_by_day_in_week.values[5],
+    flights_by_day_in_week.values[4],
+    flights_by_day_in_week.values[1],
+    flights_by_day_in_week.values[0],
+    flights_by_day_in_week.values[6],
+    flights_by_day_in_week.values[3]
+]
+plt.figure(figsize=(10, 2))
+sns.set(style="darkgrid")
+sns.barplot(days, values_by_day, color="lightblue", alpha=0.8)
+plt.title("Vuelos por Día de la Semana")
+plt.ylabel("Vuelos", fontsize=12)
+plt.xlabel("Día de la Semana", fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
+
+# 2. Generación de Características
+# 2.a. Periodo del Día
 def get_period_day(date):
     date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
     morning_min = datetime.strptime("05:00", '%H:%M').time()
@@ -42,6 +101,7 @@ def get_period_day(date):
 
 data['period_day'] = data['Fecha-I'].apply(get_period_day)
 
+# 2.b. Temporada Alta
 def is_high_season(fecha):
     fecha_año = int(fecha.split('-')[0])
     fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
@@ -64,6 +124,7 @@ def is_high_season(fecha):
 
 data['high_season'] = data['Fecha-I'].apply(is_high_season)
 
+# 2.c. Diferencia en Minutos
 def get_min_diff(data):
     fecha_0 = datetime.strptime(data['Fecha_0'], '%Y-%m-%d %H:%M:%S')
     fecha_1 = datetime.strptime(data['Fecha_1'], '%Y-%m-%d %H:%M:%S')
@@ -72,175 +133,211 @@ def get_min_diff(data):
 
 data['min_diff'] = data.apply(get_min_diff, axis=1)
 
+# 2.d. Retraso
 threshold_in_minutes = 15
 data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
 
-# Clase para manejar el modelo de retraso
-class DelayModel:
+# 3. Análisis de Datos: Segunda Vista
+# Tasa de retraso por columna
+def get_rate_from_column(data, column):
+    delays = {}
+    for _, row in data.iterrows():
+        if row['delay'] == 1:
+            if row[column] not in delays:
+                delays[row[column]] = 1
+            else:
+                delays[row[column]] += 1
 
-    TOP_FEATURES = [
-        "OPERA_Latin American Wings", 
-        "MES_7",
-        "MES_10",
-        "OPERA_Grupo LATAM",
-        "MES_12",
-        "TIPOVUELO_I",
-        "MES_4",
-        "MES_11",
-        "OPERA_Sky Airline",
-        "OPERA_Copa Air"
-    ]
-
-    DATA_SPLITTING_RANDOM_STATE = 42
-    TEST_SIZE = 0.33
-
-    MODEL_RANDOM_STATE = 1
-    LEARNING_RATE = 0.01
-
-    DELAY_THRESHOLD_MINUTES = 15
-
-    MODEL_FILE_NAME = Path("model.json")
-    MODEL_PATH = Path("models")
-
-    def __init__(self):
-        self._model: Union[BaseEstimator, ClassifierMixin] = self._load_model(
-            self.complete_model_path
-        )
-        self.target_column = None
-
-    @property
-    def complete_model_path(self) -> Path:
-        return self.MODEL_PATH / self.MODEL_FILE_NAME
-
-    def _get_minute_diff(self, data: pd.DataFrame) -> pd.Series:
-        try:
-            fecha_o = pd.to_datetime(data["Fecha-O"])
-            fecha_i = pd.to_datetime(data["Fecha-I"])
-            return (fecha_o - fecha_i).dt.total_seconds() / 60
-        except (ValueError, KeyError) as e:
-            raise ValueError("Datos de entrada o formato de fecha inválidos") from e
-
-    def _create_one_hot_features(self, data: pd.DataFrame) -> pd.DataFrame:
-        one_hot_features = pd.concat(
-            [
-                pd.get_dummies(data["OPERA"], prefix="OPERA"),
-                pd.get_dummies(data["TIPOVUELO"], prefix="TIPOVUELO"),
-                pd.get_dummies(data["MES"], prefix="MES"),
-            ],
-            axis=1,
-        )
-        return one_hot_features
-
-    def _create_delay_target(
-        self, data: pd.DataFrame, target_column: str
-    ) -> pd.DataFrame:
-        data["min_diff"] = self._get_minute_diff(data)
-        delay_target = np.where(data["min_diff"] > self.DELAY_THRESHOLD_MINUTES, 1, 0)
-        return pd.DataFrame({target_column: delay_target}, index=data.index)
-
-    def _load_model(self, model_path: Path) -> Union[BaseEstimator, ClassifierMixin]:
-        logging.info(f"Cargando modelo desde {model_path}")
-
-        try:
-            if not model_path.is_file():
-                raise FileNotFoundError(f"No se encontró ningún archivo en {model_path}")
-
-            if model_path.suffix.lower() != ".json":
-                logging.warning(
-                    f"El archivo {model_path} no tiene una extensión .json. Intentando cargar de todas formas."
-                )
-
-            model = XGBClassifier()
-            model.load_model(str(model_path))
-
-            logging.info("Modelo cargado exitosamente")
-
-            return model
-
-        except FileNotFoundError as e:
-            logging.error(f"Archivo del modelo no encontrado: {e}")
-        except json.JSONDecodeError:
-            logging.error(
-                f"Error al decodificar JSON desde {model_path}. Asegúrate de que sea un archivo JSON válido."
-            )
-        except PermissionError:
-            logging.error(f"Permiso denegado al intentar leer {model_path}")
-        except Exception as e:
-            logging.error(f"Error al cargar el modelo: {str(e)}")
-
-    def _calculate_class_weights(self, target: pd.Series) -> dict:
-        classes = np.unique(target)
-        weights = compute_class_weight(
-            class_weight="balanced", classes=classes, y=target
-        )
-        class_weights = dict(zip(classes, weights))
-        print(class_weights)
-        return class_weights
-
-    def preprocess(
-        self, data: pd.DataFrame, target_column: str = None
-    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
-        target = None
-
-        if target_column:
-            self.target_column = target_column
-            target = self._create_delay_target(data, target_column)
-
-        features = self._create_one_hot_features(data)
-        features = features.reindex(columns=self.TOP_FEATURES, fill_value=0)
-
-        if target_column:
-            return features, target
+    total = data[column].value_counts().to_dict()
+    rates = {}
+    for name, total_count in total.items():
+        if name in delays:
+            rates[name] = round((delays[name] / total_count) * 100, 2)
         else:
-            return features
+            rates[name] = 0
 
-    def fit(self, features: pd.DataFrame, target: pd.DataFrame) -> None:
-        x_train, x_test, y_train, y_test = train_test_split(
-            features,
-            target,
-            test_size=self.TEST_SIZE,
-            random_state=self.DATA_SPLITTING_RANDOM_STATE,
-        )
+    return pd.DataFrame.from_dict(rates, orient='index', columns=['Tasa (%)'])
 
-        target_series = target[self.target_column]
-        class_weights = self._calculate_class_weights(target=target_series)
-        scale_pos_weight = class_weights[1] / class_weights[0]
-        print(scale_pos_weight)
+# Tasa de retraso por destino
+destination_rate = get_rate_from_column(data, 'SIGLADES')
+destination_rate_values = data['SIGLADES'].value_counts().index
+plt.figure(figsize=(20, 5))
+sns.set(style="darkgrid")
+sns.barplot(destination_rate_values, destination_rate['Tasa (%)'], alpha=0.75)
+plt.title('Tasa de Retraso por Destino')
+plt.ylabel('Tasa de Retraso [%]', fontsize=12)
+plt.xlabel('Destino', fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
 
-        model = XGBClassifier(
-            random_state=self.MODEL_RANDOM_STATE,
-            learning_rate=self.LEARNING_RATE,
-            scale_pos_weight=scale_pos_weight,
-        )
+# Tasa de retraso por aerolínea
+airlines_rate = get_rate_from_column(data, 'OPERA')
+airlines_rate_values = data['OPERA'].value_counts().index
+plt.figure(figsize=(20, 5))
+sns.set(style="darkgrid")
+sns.barplot(airlines_rate_values, airlines_rate['Tasa (%)'], alpha=0.75)
+plt.title('Tasa de Retraso por Aerolínea')
+plt.ylabel('Tasa de Retraso [%]', fontsize=12)
+plt.xlabel('Aerolínea', fontsize=12)
+plt.xticks(rotation=90)
+plt.show()
 
-        model.fit(x_train, y_train)
+# Tasa de retraso por mes
+month_rate = get_rate_from_column(data, 'MES')
+month_rate_values = data['MES'].value_counts().index
+plt.figure(figsize=(20, 5))
+sns.set(style="darkgrid")
+sns.barplot(month_rate_values, month_rate['Tasa (%)'], color='blue', alpha=0.75)
+plt.title('Tasa de Retraso por Mes')
+plt.ylabel('Tasa de Retraso [%]', fontsize=12)
+plt.xlabel('Mes', fontsize=12)
+plt.xticks(rotation=90)
+plt.ylim(0, 10)
+plt.show()
 
-        logging.info("Entrenamiento finalizado, calculando métricas de prueba...")
+# Tasa de retraso por día
+days_rate = get_rate_from_column(data, 'DIANOM')
+days_rate_value = data['DIANOM'].value_counts().index
+plt.figure(figsize=(20, 5))
+sns.set(style="darkgrid")
+sns.barplot(days_rate_value, days_rate['Tasa (%)'], color='blue', alpha=0.75)
+plt.title('Tasa de Retraso por Día')
+plt.ylabel('Tasa de Retraso [%]', fontsize=12)
+plt.xlabel('Día', fontsize=12)
+plt.xticks(rotation=90)
+plt.ylim(0, 10)
+plt.show()
 
-        y_pred = model.predict(x_test)
-        conf_matrix = confusion_matrix(y_test, y_pred)
-        class_report = classification_report(y_test, y_pred)
-        logging.info(f"Matriz de Confusión:\n{conf_matrix}")
-        logging.info(f"Reporte de Clasificación:\n{class_report}")
+# 4. Entrenamiento
+# 4.a. División de Datos (Entrenamiento y Validación)
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix, classification_report
 
-        self.complete_model_path.parent.mkdir(parents=True, exist_ok=True)
-        model.save_model(self.complete_model_path)
-        self._model = model
+# Asegúrate de que los datos estén bien mezclados
+training_data = shuffle(data[['OPERA', 'MES', 'TIPOVUELO', 'SIGLADES', 'DIANOM', 'delay']], random_state=111)
 
-       def predict(self, features: pd.DataFrame) -> List[int]:
-        """
-        Predice retrasos para nuevos vuelos.
+# Crear variables dummy para las características categóricas
+features = pd.concat([
+    pd.get_dummies(data['OPERA'], prefix='OPERA'),
+    pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
+    pd.get_dummies(data['MES'], prefix='MES')
+], axis=1)
 
-        Args:
-            features (pd.DataFrame): datos preprocesados.
+# Definir el objetivo
+target = data['delay']
 
-        Returns:
-            (List[int]): objetivos predichos.
-        """
-        if self._model is None:
-            logging.warning("No se encontró el modelo.")
-            self._load_model(self.complete_model_path)
+# Dividir los datos en conjuntos de entrenamiento y validación
+x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=42)
 
-        predictions = self._model.predict(features)
-        return predictions.tolist()
+# Verificar la forma de los conjuntos de datos
+print(f"train shape: {x_train.shape} | test shape: {x_test.shape}")
+
+# Verificar la distribución de la variable objetivo en los conjuntos de entrenamiento y validación
+print(y_train.value_counts(normalize=True) * 100)
+print(y_test.value_counts(normalize=True) * 100)
+
+# 4.b. Selección de Modelos
+# 4.b.i. XGBoost
+import xgboost as xgb
+
+# Crear y entrenar el modelo XGBoost
+xgb_model = xgb.XGBClassifier(random_state=1, learning_rate=0.01)
+xgb_model.fit(x_train, y_train)
+
+# Guardar el modelo entrenado en un archivo .pkl
+joblib.dump(xgb_model, "challenge/finalized_model.pkl")
+
+# Realizar predicciones
+xgboost_y_preds = xgb_model.predict(x_test)
+xgboost_y_preds = [1 if y_pred > 0.5 else 0 for y_pred in xgboost_y_preds]
+
+# Evaluar el modelo
+print(confusion_matrix(y_test, xgboost_y_preds))
+print(classification_report(y_test, xgboost_y_preds))
+
+# 4.b.ii. Regresión Logística
+from sklearn.linear_model import LogisticRegression
+
+# Crear y entrenar el modelo de Regresión Logística
+reg_model = LogisticRegression()
+reg_model.fit(x_train, y_train)
+
+# Realizar predicciones
+reg_y_preds = reg_model.predict(x_test)
+
+# Evaluar el modelo
+print(confusion_matrix(y_test, reg_y_preds))
+print(classification_report(y_test, reg_y_preds))
+
+# 5. Análisis de Datos: Tercera Vista
+# Importancia de las Características
+plt.figure(figsize=(10, 5))
+plot_importance(xgb_model)
+plt.show()
+
+# Seleccionar las 10 características más importantes
+top_10_features = [
+    "OPERA_Latin American Wings",
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+]
+
+# Balance de Datos
+n_y0 = len(y_train[y_train == 0])
+n_y1 = len(y_train[y_train == 1])
+scale = n_y0 / n_y1
+print(scale)
+
+# 6. Entrenamiento con Mejora
+# 6.a. División de Datos
+x_train2, x_test2, y_train2, y_test2 = train_test_split(features[top_10_features], target, test_size=0.33, random_state=42)
+
+# 6.b. Selección de Modelos
+# 6.b.i. XGBoost con Importancia de Características y Balance
+xgb_model_2 = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight=scale)
+xgb_model_2.fit(x_train2, y_train2)
+xgboost_y_preds_2 = xgb_model_2.predict(x_test2)
+print(confusion_matrix(y_test2, xgboost_y_preds_2))
+print(classification_report(y_test2, xgboost_y_preds_2))
+
+# 6.b.ii. XGBoost con Importancia de Características pero sin Balance
+xgb_model_3 = xgb.XGBClassifier(random_state=1, learning_rate=0.01)
+xgb_model_3.fit(x_train2, y_train2)
+xgboost_y_preds_3 = xgb_model_3.predict(x_test2)
+print(confusion_matrix(y_test2, xgboost_y_preds_3))
+print(classification_report(y_test2, xgboost_y_preds_3))
+
+# 6.b.iii. Regresión Logística con Importancia de Características y Balance
+reg_model_2 = LogisticRegression(class_weight={1: n_y0/len(y_train), 0: n_y1/len(y_train)})
+reg_model_2.fit(x_train2, y_train2)
+reg_y_preds_2 = reg_model_2.predict(x_test2)
+print(confusion_matrix(y_test2, reg_y_preds_2))
+print(classification_report(y_test2, reg_y_preds_2))
+
+# 6.b.iv. Regresión Logística con Importancia de Características pero sin Balance
+reg_model_3 = LogisticRegression()
+reg_model_3.fit(x_train2, y_train2)
+reg_y_preds_3 = reg_model_3.predict(x_test2)
+print(confusion_matrix(y_test2, reg_y_preds_3))
+print(classification_report(y_test2, reg_y_preds_3))
+
+# 7. Conclusiones de Ciencia de Datos
+print("""
+Conclusiones de Ciencia de Datos:
+
+1. No hay una diferencia notable en los resultados entre XGBoost y Regresión Logística.
+2. No disminuye el rendimiento del modelo al reducir las características a las 10 más importantes.
+3. Mejora el rendimiento del modelo al balancear las clases, ya que aumenta el recall de la clase "1".
+
+Con esto, el modelo a ser productivo debe ser el que se entrena con las 10 características principales y balanceo de clases.
+""")
+
 
